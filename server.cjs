18dba62cecdccf0dd6dc3a0b7313228fe23cf878
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-15 11:15:23 -0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -38,9 +38,9 @@ app.post('/api/login', async (req, res) => {
     // Sanitize the email for safe string interpolation
     const sanitizedEmail = format('%s', email);  
     // Retrieve user from the database
-    const loginQuery = 'SELECT * FROM users WHERE email = ${sanitizedEmail}';
+    const loginQuery = 'SELECT * FROM users WHERE email = $1';
     console.log('loginQuery: ', loginQuery);
-    const result = await client.query(loginQuery);
+    const result = await client.query(loginQuery, [sanitizedEmail]);
     const user = result.rows[0];
     
     // Check if the user exists and the password is correct
