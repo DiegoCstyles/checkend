03591620d16d51beaff84f6d3d374c55ced791fa
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-11-17 15:57:26 -0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -175,7 +175,7 @@ app.get('/api/riskItemsUsage', async (req, res) => {
         const fetchQuery = `
             SELECT id, likelihood, impact, date
             FROM risk_items
-            WHERE EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE);
+            WHERE DATE_PART('year', date) = DATE_PART('year', CURRENT_DATE);
         `;
         console.log('fetchQuery: ', fetchQuery);
         const result = await client.query(fetchQuery);
